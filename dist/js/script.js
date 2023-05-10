9debcf9dc579a99c0fb9f4d1409a8fe661970f53
Lornz18 <mellgodwinbarza22@gmail.com>
2023-05-10 15:45:02 +0800

    responsive

--- dist/js/script.js
+++ dist/js/script.js
@@ -6,3 +6,22 @@ toggleMenu.addEventListener("click", () => {
   headNav.classList.toggle("open");
   blurr.classList.toggle("open");
 });
+
+const links = document.querySelectorAll(".links__drop__item h4");
+links.forEach((baba) => {
+  baba.addEventListener("click", () => {
+    baba.nextElementSibling.classList.toggle("open");
+    baba.querySelector("i").classList.toggle("open");
+  });
+});
+
+const header = document.querySelector(".header");
+const brand = document.querySelector(".branding img");
+window.addEventListener("scroll", () => {
+  console.log(window.pageYOffset);
+  if (window.pageYOffset >= 100) {
+    header.classList.add("blue");
+  } else {
+    header.classList.remove("blue");
+  }
+});
